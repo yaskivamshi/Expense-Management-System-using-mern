c0b7bac34aca8ef33f3376dcf99cffb1445f7fab
Yaski Vamshi <165493760+yaskivamshi@users.noreply.github.com>
2025-03-12 21:53:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -50,9 +50,10 @@ app.use("/api/expenses", require("./routes/expenseRoutes"));
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
 
 // Test API Route
 app.get("/", (req, res) => {
